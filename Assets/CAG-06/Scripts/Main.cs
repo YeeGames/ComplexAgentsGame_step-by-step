930ee0449cc91bf1fb340c5bbebd66798d76158e
Ethan Lin <ElementsPhoenix@outlook.com>
2023-02-28 21:48:43 +0800

    manage 2023-02-28 21:49:05
    重命名CAG_06之文件夹名和命名空间名；
    复制CAG_05；

--- Assets/CAG-06/Scripts/Main.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace CAG.S06
-{
-    public class Main : MonoBehaviour
-    {
-        
-    }
-}
---
+++ Assets/CAG_06/Scripts/Manager/CameraManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using CAG_06.Settings;
+
+namespace CAG_06
+{
+    public class CameraManager : MonoBehaviour
+    {
+        public GameSettings gameSettings;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            /// 设置主摄像机视口大小
+            this.gameObject.GetComponent<Camera>().orthographicSize = gameSettings.stageRadiu + 2;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
